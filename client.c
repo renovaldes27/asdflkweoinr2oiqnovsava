840857a0956290c41a2b3230afba9a13ee4fd469
Hazza Alkaabi <lithon@outlook.com>
2018-09-13 20:26:11 -0700

    Add comments to the client file

--- client.c
+++ client.c
@@ -1,3 +1,28 @@
+/*
+    Authors: Reno Valdes, Hazza Alkaabi
+    Date: September 14, 2018
+    Instructor: Patrick Homer
+    Computer Science 425: Principles of Networking
+    University of Arizona
+
+    Project: Program 2: Mobile TCP Proxy - Milestone 1
+    File name: client.c
+    Description: This program (the client) will write the server user input from stdin.
+                 The server in turn will print the client message to its stdout. The client
+                 is limited to 1024 charcaters. The message will be sent without the null
+                 terminator and the newline characters.
+    
+    The command line argument for the client is as follow:
+    ./client server_ip_address port_number
+    where:
+    server_ip_address is the ip address of the server.  
+    port_number is the port number that the server will be listneing to.
+
+    Note: A Makefile is provided. 
+    Run "make" or "make all" in the command line in the directory where Makefile are located.
+    The Makefile will clean first, then compile both of the client and server source files.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -10,12 +35,16 @@
 #include <arpa/inet.h>
 
 
-// connectToSock
-// params: int port : the port to connect to
-//         string hostName : the host to connect to
-// return: int sock_desc
-// This function takes a port number and host name
-// and connects to a socket at that location.
+/*
+    connectToSock
+
+    params: int port : the port to connect to
+            string hostName : the host to connect to
+    return: int sock_desc
+    
+    This function takes a port number and an ip address
+    and connects to a socket at that location.
+*/
 int connectToSock(int port, const char* ipString){
     int sock_desc;
     sock_desc = socket(PF_INET, SOCK_STREAM, 6);
@@ -25,6 +54,7 @@ int connectToSock(int port, const char* ipString){
     sin.sin_family = PF_INET;
     sin.sin_port = htons(port);
     in_addr_t address;
+    // Convert ip
     address = inet_addr(ipString);
     if ( address == -1 ) {
          fprintf(stderr, "ERROR: inet_addr failed to convert ipString '%s'\n", ipString);
@@ -40,6 +70,18 @@ int connectToSock(int port, const char* ipString){
     return sock_desc;
 }
 
+/*
+    queryServer
+
+    params: int sock_desc : the socket to connect to
+
+    return: int sock_desc
+    
+    This function waits for user input from stdin
+    to send it to the server. It is maxed at 1024,
+    that is without null terminator and newline
+    characters.
+*/
 void queryServer(int sock_desc){  
     while(1){
         size_t BUFLEN = 1024;
@@ -51,18 +93,24 @@ void queryServer(int sock_desc){
             return;
         }
 
-        if (n > 1025){ // 1025 to account for newline character, so max char sent is 1024
+        // 1025 to account for newline character, so max char sent is 1024
+        if (n > 1025){
             n = 1025;
         }
 
-        int num = n-1;  // Don't write the newline character
+        // Don't write the newline character
+        int num = n-1;
 
+        // change num to to a network value to send it
         num = htonl(num);
+
+        // write the size of the buffer
         int result = write(sock_desc, &num, sizeof(num));
         if (result < 0){
             fprintf(stderr, "ERROR: Failed to write to server\n");
         }
-        
+
+        // write the user text to the server
         result = write(sock_desc, buf, n-1);
 
         if (result < 0){
@@ -78,7 +126,7 @@ int main( int argc, const char* argv[] )
         fprintf(stderr,"ERROR: Too many cmd-line arguments\n");
         exit(1);
     }
-
+    
     int port = atoi(argv[2]);
     int sock_desc = connectToSock(port,argv[1]);
 
